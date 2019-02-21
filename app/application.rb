f66b6c5e3e15200a7040444666827b422bfa52ec
Lauren Pardee <lap5151@gmail.com>
2019-02-21 00:56:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     req = Rack::Request.new(env)
 
     search_item = req.params["q"]
-    
+
   if req.path.match(/items/)
     if @@items.include?(search_item)
       resp.write "#{search_item} is one of our items"
